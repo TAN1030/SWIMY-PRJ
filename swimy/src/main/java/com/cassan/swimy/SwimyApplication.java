f997aa87d74a0dc8793f43327568b1aaccc578fa
TAN1030 <tangerine.k@outlook.kr>
2023-10-31 19:35:04 +0900

    init

---
+++ swimy/src/main/java/com/cassan/swimy/SwimyApplication.java
@@ -0,0 +1,13 @@
+package com.cassan.swimy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimyApplication.class, args);
+	}
+
+}
